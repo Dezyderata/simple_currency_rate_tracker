837c42566f0810d8ddca875eb0bc9fbf4e5d6c54
Dezyderata <agnieszkarydzyk@gmail.com>
2023-03-30 15:02:05 +0200

    Security change

--- simple_currency_rate_tracker/simple_currency_rate_tracker/settings.py
+++ simple_currency_rate_tracker/simple_currency_rate_tracker/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_9!^0xn=k#-ap-c7vmye1v8%c&*n8mx6mj#h^w35)df@(($6=u'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
